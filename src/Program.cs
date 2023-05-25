c3636610c5e023b5288530022ef3c3a388e30e60
Tim Heuer <tim@timheuer.com>
2023-05-25 14:21:11 -0700

    Fix invalid repo exception handling

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,20 @@
 ﻿// get the current working directory
 using LibGit2Sharp;
+using System.Runtime.CompilerServices;
 
 string path = Directory.GetCurrentDirectory();
-using (var repo = new Repository(path))
+
+try
 {
-    Console.WriteLine($"Repository is at {repo.Info.WorkingDirectory}");
-    Console.WriteLine($"Branch is {repo.Head.FriendlyName}");
-    Console.WriteLine($"Upstream branch is {repo.Head.TrackedBranch.FriendlyName}");
-    Console.WriteLine($"Has uncommitted changes: {repo.RetrieveStatus().IsDirty}");
+    using (var repo = new Repository(path))
+    {
+        Console.WriteLine($"Repository is at {repo.Info.WorkingDirectory}");
+        Console.WriteLine($"Branch is {repo.Head.FriendlyName}");
+        Console.WriteLine($"Upstream branch is {repo.Head.TrackedBranch.FriendlyName}");
+        Console.WriteLine($"Has uncommitted changes: {repo.RetrieveStatus().IsDirty}");
+    }
+}
+catch (RepositoryNotFoundException ex)
+{
+    Console.WriteLine($"Not a valid repository root: {path}");
 }
