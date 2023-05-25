6bea1a56f4111564eba0d015fb578b31bfb05866
Tim Heuer <tim@timheuer.com>
2023-05-25 14:47:31 -0700

    Update release notes and version for displaying remote URL info

--- src/Program.cs
+++ src/Program.cs
@@ -8,8 +8,12 @@ try
 {
     using (var repo = new Repository(path))
     {
+        var headRemote = repo.Head.RemoteName;
+        var remote = repo.Network.Remotes.FirstOrDefault(r => r.Name == headRemote);
         Console.WriteLine($"Repository is at {repo.Info.WorkingDirectory}");
         Console.WriteLine($"Branch is {repo.Head.FriendlyName}");
+        Console.WriteLine($"Remote is {headRemote}");
+        Console.WriteLine($"Remote URL is {remote.Url}");
         Console.WriteLine($"Upstream branch is {repo.Head.TrackedBranch.FriendlyName}");
         Console.WriteLine($"Has uncommitted changes: {repo.RetrieveStatus().IsDirty}");
     }
