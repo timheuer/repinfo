3063ff4d2efddcd623a88c68affb53ab23e4b4b0
Tim Heuer <tim@timheuer.com>
2023-05-25 13:56:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// get the current working directory
+using LibGit2Sharp;
+
+string path = Directory.GetCurrentDirectory();
+using (var repo = new Repository(path))
+{
+    Console.WriteLine($"Repository is at {repo.Info.WorkingDirectory}");
+    Console.WriteLine($"Branch is {repo.Head.FriendlyName}");
+    Console.WriteLine($"Upstream branch is {repo.Head.TrackedBranch.FriendlyName}");
+    Console.WriteLine($"Has uncommitted changes: {repo.RetrieveStatus().IsDirty}");
+}
